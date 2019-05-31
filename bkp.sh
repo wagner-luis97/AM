6881652b57a03ea9be0e0031b61d70471036d261
mordecai <32494095+wagner-luis97@users.noreply.github.com>
2019-05-31 20:24:05 -0300

    Add files via upload

---
+++ bkp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+read $comand
+echo " Welcome to the Backup Center ! "
+sleep 5
+clear
+echo " Type the required informations ... "
+sleep 5 
+clear
+echo " Type the command to compress your file for backup : " $comand " " 
+sleep 30
+clear
+echo " Give the permissions for the compress file : " $comand " "
+sleep 20
+clear
+echo " Now, you must copy the file to a folder of your preference ! "
+echo " Type the command : " $comand " "
+sleep 25
+clear
+echo " Processing ... "
+sleep 5
+clear
+echo " Done !!! "
+
